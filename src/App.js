f779585854541145550a49db55a2f99d5e3ebb04
yoshivn1203 <yoshivn1203@gmail.com>
2022-09-25 21:29:24 +0700

    basic router + navbar + footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,49 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { Navbar, Sidebar, Footer } from './components'
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Navbar, Sidebar, Footer } from './components';
+import {
+  About,
+  AuthWrapper,
+  Cart,
+  Checkout,
+  Error,
+  Home,
+  PrivateRoute,
+  Products,
+  SingleProduct,
+} from './pages';
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return (
+    <Router>
+      <Navbar />
+      <Sidebar />
+      <Switch>
+        <Route path='/' exact>
+          <Home />
+        </Route>
+        <Route path='/about' exact>
+          <About />
+        </Route>
+        <Route path='/cart' exact>
+          <Cart />
+        </Route>
+        <Route path='/products' exact>
+          <Products />
+        </Route>
+        <Route path='/products/:id' exact>
+          <SingleProduct />
+        </Route>
+        <Route path='/checkout' exact>
+          <Checkout />
+        </Route>
+        <Route path='*' exact>
+          <Error />
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
+  );
 }
 
-export default App
+export default App;
